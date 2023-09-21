bab07674b64beb265ede7cc45e0ba915abc76a5f
Fabiana Limma <fabianallima@hotmail.com>
2023-09-21 17:14:05 -0300

    Created a structure that contains the possible stories that the user can choose from

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,22 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let storyLabel0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    let stories = [
+       Story(title: "You see a fork in the roud", choice1: "Take a left.", choice2: "Take a right."),
+       Story(title: "You see a tiger ", choice1: "Shoult for help", choice2: "play dead."),
+       Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps."),
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = storyLabel0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        let getStory = stories[0]
+        storyLabel.text = getStory.title
+        choice1Button.setTitle(getStory.choice1, for: .normal)
+        choice2Button.setTitle(getStory.choice2, for: .normal)
     }
-
-
+    
+    
 }
 
